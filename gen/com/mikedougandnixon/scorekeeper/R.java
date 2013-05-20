bbf975409bc21342d48a42b78374def898a7d8d5
Michael Hoyle <mah68@duke.edu>
2013-05-20 17:59:03 -0400

    initial

--- gen/com/mikedougandnixon/scorekeeper/R.java
+++ gen/com/mikedougandnixon/scorekeeper/R.java
@@ -12,25 +12,30 @@ public final class R {
     }
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
+        public static final int icon=0x7f020001;
+        public static final int score_down_button=0x7f020002;
+        public static final int score_up_button=0x7f020003;
     }
     public static final class id {
-        public static final int addPlayerButton=0x7f050006;
+        public static final int addPlayerButton=0x7f050005;
+        public static final int confirmButton=0x7f05000b;
         public static final int currentHoleTextView=0x7f050001;
-        public static final int holeScoreTextView=0x7f050009;
+        public static final int denyButton=0x7f05000c;
+        public static final int holeScoreTextView=0x7f050008;
         public static final int nextHoleButton=0x7f050002;
-        public static final int playerNameEditText=0x7f050005;
-        public static final int playerNameTextView=0x7f050008;
+        public static final int playerNameEditText=0x7f050004;
+        public static final int playerNameTextView=0x7f050007;
         public static final int playersListView=0x7f050003;
         public static final int previousHoleButton=0x7f050000;
-        public static final int scoreDownButton=0x7f05000b;
+        public static final int scoreDownButton=0x7f050009;
         public static final int scoreUpButton=0x7f05000a;
-        public static final int textView1=0x7f050004;
-        public static final int totalScoreTextView=0x7f050007;
+        public static final int totalScoreTextView=0x7f050006;
     }
     public static final class layout {
         public static final int main=0x7f030000;
         public static final int playernamedialog=0x7f030001;
         public static final int playerview=0x7f030002;
+        public static final int removeplayerdialog=0x7f030003;
     }
     public static final class string {
         public static final int app_name=0x7f040001;
